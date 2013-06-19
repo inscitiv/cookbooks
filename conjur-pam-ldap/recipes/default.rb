bf7e53a3b3158607e8112c6c4ddafd72619f5a6b
Kevin Gilpin <kgilpin@gmail.com>
2013-06-18 20:25:51 -0400

    pam+ldap cookbook updates from VM testing

--- conjur-pam-ldap/recipes/default.rb
+++ conjur-pam-ldap/recipes/default.rb
@@ -1,8 +1,10 @@
 chef_gem "netrc"
 
 # Read the netrc and find the entry for the conjur authn endpoint
+require 'netrc'
 netrc = Netrc.read
-
+user, password = netrc['https://authn-ci-conjur.herokuapp.com']
+host = user.split('/')[1..-1].join('/')
 
 # Answer the installer questions about LDAP server location, root name, etc
 template "/tmp/ldap.seed" do
@@ -28,20 +30,40 @@ for s in %w(nscd nslcd ssh)
 	end
 end
 
+[ [ 'users', 100, 5000 ], [ 'admin', 111, 50000 ] ].each do |group|
+  name, src, dest = group
+  group name do
+    gid dest
+    action :modify
+  end
+  execute "change gid of #{src} to #{dest}" do
+   command "find / -gid #{src} -exec chgrp #{dest} {} +"
+   only_if do
+     require 'etc'
+     begin
+       Etc.getgrnam(name).gid == src
+     rescue ArgumentError
+       false
+     end
+   end
+  end
+end
+
 ruby_block "Enable ssh password authentication" do
   block do
     file = Chef::Util::FileEdit.new("/etc/ssh/sshd_config")
     file.search_file_replace_line(/PasswordAuthentication\s+no/, "PasswordAuthentication yes")
     file.write_file    
   end
+  notifies :restart, [ "service[ssh]" ]
 end
 
 template "/etc/nslcd.conf" do
 	source "nslcd.conf.erb"
-	variables uri: "",
-	 base: "",
-	 binddn: "",
-	 bindpw: ""
+	variables uri: "ldap://ec2-23-20-251-229.compute-1.amazonaws.com:1389",
+	 base: "ou=user,host=#{host},account=ci,o=conjur",
+	 binddn: "uid=#{host},ou=host,host=#{host},account=ci,o=conjur",
+	 bindpw: password
 	notifies :restart, [ "service[nscd]", "service[nslcd]" ]
 end
 
